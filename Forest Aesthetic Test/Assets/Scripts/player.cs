61102cd20eb58ec1d83867942c910ae9fc75e199
chasetrox <chasetrox@gmail.com>
2018-03-27 13:37:43 -0400

    Added Alex's demo environment

---
+++ Forest Aesthetic Test/Assets/Scripts/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class player : NetworkBehaviour {
+
+    public static player hunter;
+    public bool isHunter;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+            Destroy(gameObject);
+	}
+
+    public override void OnStartClient()
+    {
+        if (hunter == null)
+        {
+            hunter = this;
+            isHunter = true;
+        }
+        else
+            isHunter = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Forest Aesthetic Test/Assets/Scripts/playerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerManager : NetworkBehaviour {
+
+    public GameObject HunterPrefab;
+    public GameObject BeastPrefab;
+
+    private GameObject localPlayer;
+    private GameObject hunter;
+    private GameObject beast;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public override void OnStartClient()
+    {
+        localPlayer = ClientScene.localPlayers[0].gameObject;
+
+        if (localPlayer.GetComponent<player>().isHunter)
+        {
+            hunter = localPlayer;
+            Instantiate(BeastPrefab);
+        }
+        else
+            beast = localPlayer;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isServer)
+            return;
+
+        if (hunter != null)
+        {
+            
+        }
+	}
+}
